ad3ae2ef13217865d94c73361370f9eb8b29099e
DQNEO <dqneoo@gmail.com>
2017-04-10 20:34:55 +0900

    extract func

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -4,16 +4,20 @@ import "github.com/DQNEO/go-FizzBuzzEnterpriseEdition/logic"
 import "fmt"
 
 
-func main() {
-    start := 1
-    end := 30
-
+func getSlice(start int, end int) []int {
     numbers := make([]int, end, end)
 
     for i := start  - 1; i < end ; i++ {
         numbers[i] = i + 1
     }
+    return numbers
+}
 
+func main() {
+    start := 1
+    end := 30
+
+    numbers := getSlice(start, end)
     for _,v := range(numbers) {
         s := logic.Logic(v)
         fmt.Println(s.String())
